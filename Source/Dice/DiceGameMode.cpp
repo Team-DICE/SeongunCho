4690fa41ccb2cd3bb1253997c38c3c670934590b
whddlzjq <160713866+whddlzjq@users.noreply.github.com>
2024-04-28 22:02:56 +0900

    2

--- Source/Dice/DiceGameMode.cpp
+++ Source/Dice/DiceGameMode.cpp
@@ -3,8 +3,6 @@
 #include "DiceGameMode.h"
 #include "DiceCharacter.h"
 #include "UObject/ConstructorHelpers.h"
-
-#include "SMainMenuWidget.h"
 #include "Widgets/SWeakWidget.h"
 #include "Framework/Application/SlateApplication.h"
 
